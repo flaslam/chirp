b03a97a78f7abcc6ff3ccd8f9f2618a6499108c3
Fahd <flaslam@gmail.com>
2022-08-03 09:13:28 +0100

    update device sizes in next.js config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ["localhost", "chirp-backend-flaslam.herokuapp.com"],
+    deviceSizes: [640, 768, 1024, 1280, 1536],
   },
 };
 
